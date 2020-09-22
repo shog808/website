6212c52e6c07e41696122dc9f72c03975ddf1d4e
shaoyucai <xiaoshao808@126.com>
2020-09-22 18:04:49 +0800

    first test

---
+++ site_config/blog.js
@@ -0,0 +1,43 @@
+export default {
+  'en-us': {
+    barText: 'Blog',
+    postsTitle: 'All posts',
+    list: [
+      {
+        title: 'The first blog',
+        author: 'author1',
+        dateStr: 'May 12nd，2018',
+        desc: 'Blog description, some text to sum up the main content of the blog',
+        link: '/en-us/blog/blog1.html',
+      },
+      {
+        title: 'The second blog',
+        author: 'author2',
+        dateStr: 'May 2nd，2018',
+        desc: 'Blog description, some text to sum up the main content of the blog',
+        link: '/en-us/blog/blog2.html',
+      },
+    ],
+  },
+  'zh-cn': {
+    barText: '博客',
+    postsTitle: '所有文章',
+    list: [
+      {
+        title: '解放军三大军机台海军演',
+        author: 'shoo',
+        dateStr: 'Sep 22nd，2020',
+        desc: '国庆前收复台湾，献礼祖国人民',
+        link: '/zh-cn/blog/blog1.html',
+      },
+      {
+        title: '中国农民丰收节',
+        author: '邵育才',
+        dateStr: 'Sep 22nd，2020',
+        desc: '秋天是丰收的季节，为充分发挥农民主体作用，突出民俗特色，弘扬悠久厚重的农耕文化，9月18日～10月10日，我市将举行“中国农民丰收节”庆祝活动。',
+        link: '/zh-cn/blog/blog2.html',
+      },
+      
+    ],
+  },
+};
